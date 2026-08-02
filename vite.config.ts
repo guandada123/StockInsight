b24bbaa20e0e05bafd39cb1d38ae48de89798dd4
guandada123 <409899032@qq.com>
2026-08-02 19:51:38 +0800

    chore: routine updates and cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   },
   envPrefix: ["VITE_", "TAURI_"],
   build: {
-    target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
+    target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "es2020",
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     sourcemap: !!process.env.TAURI_DEBUG,
   },
